aa0460266e97724f87d0551359c4e7a9db9bceb3
sainingo <nickmentor421@gmail.com>
2024-02-03 14:38:17 +0300

    Improve Portfolio

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import covid from '../assets/projects/covid.PNG';
+import srilanka from '../assets/projects/srilanka.png';
 import bookstore from '../assets/projects/bookstore.PNG';
 import netflix from '../assets/projects/netflix.PNG';
 import space from '../assets/projects/space.PNG';
@@ -22,21 +22,21 @@ const Work = () => {
 
             {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${covid})` }}
+            style={{ backgroundImage: `url(${srilanka})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-               Covid Tracker App
+               Srilanka Travel Agency
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://global-19-tracker.netlify.app/'>
+                <a href='https://srilanka-agency.vercel.app/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                    Demo
                   </button>
                 </a>
-                <a href='https://github.com/sainingo/covid-tracker-app'>
+                <a href='/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
